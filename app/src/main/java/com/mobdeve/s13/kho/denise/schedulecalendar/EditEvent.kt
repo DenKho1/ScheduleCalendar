1c49f87ce84bfb1752af3d0c65a60793e8e3655a
KDM7 <jeremiah_bool@dlsu.edu.ph>
2023-03-13 23:05:17 +0800

    Event Details Event Details Host (Incomplete) Edit Event (Not Linked)

---
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/EditEvent.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.kho.denise.schedulecalendar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class EditEvent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_event)
+    }
+}
