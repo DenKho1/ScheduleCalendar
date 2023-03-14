e9e90aaae4172062e7fafd867a6edcd6e874755e
Denise Kho <denise_jilian_kho@dlsu.edu.ph>
2023-03-14 23:38:33 +0800

    Change in Color + added code to register

--- app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/Register.kt
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/Register.kt
@@ -1,14 +1,42 @@
 package com.mobdeve.s13.kho.denise.schedulecalendar
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val register = findViewById<Button>(R.id.RegisterButton)
 
+        register.setOnClickListener {
 
+            val name = findViewById<EditText>(R.id.Rusername)
+            val password = findViewById<EditText>(R.id.Rpassword)
+            val cpassword = findViewById<EditText>(R.id.Rconfirmpassword)
+            val email = findViewById<EditText>(R.id.Remail)
+            val mobile = findViewById<EditText>(R.id.Rmobilenum)
+
+            if(name.text.isEmpty() || password.text.isEmpty() || cpassword.text.isEmpty() || email.text.isEmpty() || mobile.text.isEmpty())
+             {
+                val toast=Toast.makeText(applicationContext,"Something is missing!",Toast.LENGTH_LONG)
+                 toast.show()
+             }
+            //passwords dont match
+            else if (!password.text.equals(cpassword.text)) {
+                val toast=Toast.makeText(applicationContext,"Your Passwords' Do Not Match !",Toast.LENGTH_LONG)
+                toast.show()
+            }
+
+            else
+            {
+                val intent = Intent(this, Account::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
