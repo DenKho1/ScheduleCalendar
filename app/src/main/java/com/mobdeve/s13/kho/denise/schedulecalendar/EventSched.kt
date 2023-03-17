a9cd75d9679994aa29d0445e9c03273e29d39a8c
keepo <tairana725@gmail.com>
2023-03-17 10:38:47 +0800

    +sched

---
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/EventSched.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s13.kho.denise.schedulecalendar
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class EventSched : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_sched)
+    }
+}
