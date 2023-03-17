029bbbe2b72df30f1fceaff3e7dd10cad8d5b3e2
Denise Kho <denise_jilian_kho@dlsu.edu.ph>
2023-03-17 12:15:36 +0800

    account update with header

--- app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/Account.kt
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/Account.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -26,6 +27,13 @@ class Account : AppCompatActivity() {
 
         register.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val sched=findViewById<ImageButton>(R.id.acctosched)
+
+        sched.setOnClickListener {
+            val intent = Intent(this,EventSched::class.java)
             startActivity(intent)
         }
 
