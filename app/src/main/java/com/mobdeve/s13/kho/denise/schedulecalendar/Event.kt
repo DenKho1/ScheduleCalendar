3479fbdfd606f8e534e8b5fc1feaefe0eb0125a0
keepo <tairana725@gmail.com>
2023-03-17 17:40:55 +0800

    recyclerView no Click yet

---
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/Event.kt
@@ -0,0 +1,12 @@
+package com.mobdeve.s13.kho.denise.schedulecalendar
+
+class Event (eventName: String, eventLoc: String, eventDate: String){
+    var eventName = eventName
+    private set
+
+    var eventLoc = eventLoc
+        private set
+
+    var eventDate = eventDate
+        private set
+}
---
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/MyAdapter.kt
@@ -0,0 +1,22 @@
+package com.mobdeve.s13.kho.denise.schedulecalendar
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class MyAdapter(private val data: ArrayList<Event>): RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.activity_event_sched_layout, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bindData(data[position])
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
