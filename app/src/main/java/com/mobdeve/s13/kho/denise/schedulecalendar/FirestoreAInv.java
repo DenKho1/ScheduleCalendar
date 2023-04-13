224498270259b497c082542acb53d659b6438f5d
KDM7 <jeremiah_bool@dlsu.edu.ph>
2023-04-13 17:46:36 +0800

    attempt

--- app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/FirestoreAInv.java
+++ app/src/main/java/com/mobdeve/s13/kho/denise/schedulecalendar/FirestoreAInv.java
@@ -3,6 +3,7 @@ package com.mobdeve.s13.kho.denise.schedulecalendar;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +11,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.model.Document;
+import com.google.protobuf.Api;
 
 import javax.annotation.Nullable;
 
@@ -30,10 +37,33 @@ public class FirestoreAInv extends AppCompatActivity {
         setContentView(R.layout.activity_account);
 
         TextView uName = findViewById(R.id.AccUsername);
+        TextView uEmail = findViewById(R.id.accEmail);
+        TextView uMobile = findViewById(R.id.accMobile);
+
+
+
+        SharedPreferences sp = getSharedPreferences("USERNAME",Context.MODE_PRIVATE);
+        String named=sp.getString("NAME_KEY","Anon");
+
+//        if(named!=null) {
+//            CollectionReference userRef = FirebaseFirestore.getInstance().collection("Users")
+//                    .whereEqualTo("username", named)
+//                    .get()
+//                    .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//                        @Override
+//                        public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//                            if(!queryDocumentSnapshots.isEmpty()) {
+//                                DocumentSnapshot documentSnapshot = queryDocumentSnapshots.getDocuments().get();
+//                            }
+//                        }
+//                    });
+//
+//
+//        }
 
         setUpRecyclerView();
 
-        String name = uName.getText().toString();
+
     }
 
     private void setUpRecyclerView() {
